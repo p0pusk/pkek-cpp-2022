ae9c8c05390e1a8ec8f85cd94c2bf4181a89aed1
Son Artyom <sai@modumlab.com>
2022-05-03 12:26:26 +0300

    deleted files

--- lab-3/main.cpp
+++ lab-3/main.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
 #include <QApplication>
 #include <QtWidgets>
-#include <vector>
-#include "MenuNode.h"
-#include "MenuWindow.h"
+#include "Widget.h"
 
 
 int main(int argc, char* argv[]) {
 	QApplication app(argc, argv);
 
-	std::vector<MenuNode> data = Read("input.txt");
-	MenuWindow window(data);
-
+	Widget window("input.txt");
 	window.show();
-
 
 	return app.exec();
 }
